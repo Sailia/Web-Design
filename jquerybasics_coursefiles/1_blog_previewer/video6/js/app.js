7ec4d19dd928f2961756ae92704018f0882a09b4
Skylar <syema.ailia@gmail.com>
2018-04-15 22:59:13 -0500

    6 complete

--- jquerybasics_coursefiles/1_blog_previewer/video6/js/app.js
+++ jquerybasics_coursefiles/1_blog_previewer/video6/js/app.js
@@ -1,11 +1,24 @@
-$('#flashMessage')
-  .hide()
-  .slideDown(1000)
-  .delay(3000)
-  .slideUp();
 
-const title = "My First Blog Post";
-const content = "This is my <strong>first</strong> post!";
+// const title = "My First Blog Post";
+// const content = "This is my <strong>first</strong> post!";
 
-$("#blogTitlePreview").text(title);
-$("#blogContentPreview").html(content);
+	$('#flashMessage')
+	  .hide();
+
+$('#previewButton').click(function(){
+	const title = $('#blogTitleInput').val();
+	const content = $('#blogContentInput').val();
+	$("#blogTitlePreview").text(title);
+	$("#blogContentPreview").html(content);
+
+	$('#flashMessage')
+	  .slideDown(1000)
+	  .delay(3000)
+	  .slideUp();
+
+})
+
+
+
+
+
