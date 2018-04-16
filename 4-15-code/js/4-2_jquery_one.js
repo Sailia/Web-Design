89d2ef533d025757330a0e2bda6facc140efeead
Skylar <syema.ailia@gmail.com>
2018-04-16 15:17:33 -0500

    in class jQuery 4-15

---
+++ 4-15-code/js/4-2_jquery_one.js
@@ -0,0 +1,15 @@
+$("").click(function(){
+    $(this).hide();
+});
+
+//BUILDS
+
+    //feature 1 - simple ui screen 
+    
+    //feature 2 - CLICK TRACKER BG COLOR CHANGER
+
+    //feature 3 - MENU WITH DRAGGABLE DROP DOWN OPTIONS - DRAGGABLES ACTIVATE ANIMATION
+
+    // combine features into one DOM html 
+    
+    // call from UI screen - hide from active screen 
